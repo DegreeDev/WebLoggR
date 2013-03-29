e817fdcb19151e71bbb52426b6bed87f41b75c40
Matthew Lee <mlee@smartwire.com>
2013-03-29 10:21:52 -0500

    Added ControllerHubBase
    This class gives a controller access to a hub

---
+++ WebLoggR/Controllers/ControllerHubBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebLoggR.Controllers
+{
+    public class ApiHubBase<HubType> : ApiController
+        where HubType : WebLoggR.Hubs.GreenRHub
+    {
+        private Lazy<IHubContext> hub = new Lazy<IHubContext>(
+            () => Microsoft.AspNet.SignalR.GlobalHost.ConnectionManager.GetHubContext<HubType>()
+        );
+        public IHubContext Hub
+        {
+            get { return hub.Value; }
+        }
+    }
+    public class MvcHubBase<HubType> : Controller
+        where HubType : WebLoggR.Hubs.GreenRHub
+    {
+        private Lazy<IHubContext> hub = new Lazy<IHubContext>(
+            () => Microsoft.AspNet.SignalR.GlobalHost.ConnectionManager.GetHubContext<HubType>()
+        );
+        public IHubContext Hub
+        {
+            get { return hub.Value; }
+        }
+    }
+}
